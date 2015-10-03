592fee568e25e171b21950a01fa8ab5b5e32b8ed
Andrej Hoos <andrej.hoos@gmail.com>
2015-10-03 15:59:15 +0100

    Final adjustements

--- test2/test2.ino
+++ test2/test2.ino
@@ -7,6 +7,7 @@ void setup() {
   // initialize the robot
   Robot.begin();
   Robot.beginTFT();
+  Robot.beginSpeaker();
   
   // setup colors
   Robot.stroke(0,0,0);
@@ -24,21 +25,25 @@ int mini(uint16_t* a){
 }
 
 int n = 0;
-int renderF = 20;
+int renderF = 1000;
 
 int lastLeftMotor = 50;
 int lastRightMotor = 50;
 
 int right = 1;
 int left = -1;
+unsigned long time;
+unsigned long timeSum;
+
+int multiplier = 25;
 
 void turn(int dir) {
   if (n%renderF==0){
     Robot.text(dir == right ? "Right" : "Left", 10, 50);
   }
   //Robot.motorStop();
-  int p1 = 180;
-  int p2 = 60;
+  int p1 = 7.2*multiplier;
+  int p2 = 2.4*multiplier;
   setMotorPower(getPowerLevel(p1, p2, dir), getPowerLevel(p2, p1, dir));
   setLastMotors(getPowerLevel(p1, p2, dir), getPowerLevel(p2, p1, dir));
 }
@@ -48,8 +53,8 @@ void turnHard(int dir) {
     Robot.text(dir == right ? "Right H" : "Left H", 10, 50);
   }
   //Robot.motorStop();
-  int p1 = 160;
-  int p2 = -75;
+  int p1 = 6.4*multiplier;
+  int p2 = -3*multiplier;
   setMotorPower(getPowerLevel(p1, p2, dir), getPowerLevel(p2, p1, dir));
   setLastMotors(getPowerLevel(p1, p2, dir), getPowerLevel(p2, p1, dir));
 }
@@ -79,8 +84,12 @@ void setMotorPower(int m1, int m2){
   Robot.motorsStop();
 }
 
-void loop() {
+void loop() {  
   n++;
+  if(time != NULL){
+    timeSum += millis() - time;
+  }
+  time = millis();
   
   Robot.updateIR();
   
@@ -91,12 +100,14 @@ void loop() {
     Robot.rect(0, 0, 80, 120); 
     Robot.text(minvalue,20,10);
     Robot.text(mindex,20,20);
+    Robot.text((int)(timeSum/(n-1)),20,70);
+    Robot.text(n,20,80);
   }
 
   if (minvalue<=450){
     if (mindex==2){
-      setMotorPower(120, 120);
-      setLastMotors(120, 120);
+      setMotorPower(4.8*multiplier, 4.8*multiplier);
+      setLastMotors(4.8*multiplier, 4.8*multiplier);
     }
     else if (mindex==1){
       turn(right);
