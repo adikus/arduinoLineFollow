d20237cfa7a073d6dc43dafe53601e37e8753749
BrigadierBaobab <andreea.lutac@gmail.com>
2015-10-03 12:22:51 +0100

    Made it better. Needs refactoring.

--- test/test.ino
+++ test/test.ino
@@ -25,8 +25,6 @@
 #include <Wire.h>
 #include <SPI.h>
 
-int lastCompassVal = -1;
-int currentCompassVal;
 
 void setup() {
   // initialize the robot
@@ -46,50 +44,101 @@ int mini(uint16_t* a){
   return minidx;
 }
 
+int n = 0;
+int lastLeftMotor = -1;
+int lastRightMotor = -1;
+
 void loop() {
+  n = n + 1;
   // store the sensor information into the array
   Robot.updateIR();
-  Robot.rect(0, 0, 50, 120); 
+  if (n%10==0){
+    Robot.rect(0, 0, 80, 120); 
+  }
   // iterate the array and print the data to the Serial port
 //  for (int i = 0; i < 5; i++) {
 //    Robot.text((int)Robot.IRarray[i], 20, 20*i);
 //  }
   
-  //currentCompassVal = Robot.compassRead();
-  //Robot.text(currentCompassVal,20,20);
-  //Robot.text(lastCompassVal,20,60);
   int mindex = mini(Robot.IRarray);
-  Robot.text((int)Robot.IRarray[mindex],20,10);
-  Robot.text(mindex,20,20);
-  Robot.text(Robot.analogRead(M3),20, 40);
-  //Robot.text(mini(Robot.IRarray),30,30);
-  if (Robot.IRarray[mindex]<=300 && Robot.IRarray[mindex]>=200){
+  if (n%10==0){
+    Robot.text((int)Robot.IRarray[mindex],20,10);
+    Robot.text(mindex,20,20);
+  }
+  //Robot.text(Robot.analogRead(M3),20, 40);
+
+  if (Robot.IRarray[mindex]<=450){
     if (mindex==2){
+      if (n%10==0){
+        Robot.text("Middle", 10, 50);
+      }
       Robot.motorsWrite(75, 75);
-      //lastCompassVal = Robot.compassRead();
+      
+      lastLeftMotor = 75;
+      lastRightMotor = 75;
     }
     else if (mindex==1){
+      if (n%10==0){
+        Robot.text("Right", 10, 50);
+      }
+     // Robot.motorsStop();
+      //Robot.motorsWrite(-45, 85);
+      Robot.motorsWrite(85, -20);
+      delay(150);
       Robot.motorsStop();
-      Robot.motorsWrite(-30, 75);
+      
+      lastLeftMotor = 85;
+      lastRightMotor = -20;
     }
     else if(mindex==3){
       //Robot.turn(-5);
-      Robot.motorsStop();
-      Robot.motorsWrite(75, -30);
+      if (n%10==0){
+        Robot.text("Left", 10, 50);
+      }
+      //Robot.motorsStop();
+      //Robot.motorsWrite(85, -45);
+      Robot.motorsWrite(-20, 85);
+//      delay(150);
+//      Robot.motorsStop();
+      
+      lastLeftMotor = -20;
+      lastRightMotor = 85;
     }
-    else if (mindex=0){
+    else if (mindex==0){
       //Robot.turn(-10);
-      Robot.motorsStop();
-      Robot.motorsWrite(-50, 75);
+      if (n%10==0){
+        Robot.text("Rightmost", 10, 50);
+      }
+      //Robot.motorsStop();
+      //Robot.motorsWrite(-75, 85);
+      Robot.motorsWrite(85, -40);
+      //delay(150);
+      //Robot.motorsStop();
+      lastLeftMotor = 85;
+      lastRightMotor = -40;
     }
-    else if (mindex=4){
-      Robot.motorsStop();
-      Robot.motorsWrite(75, -50);
+    else if (mindex==4){
+      //Robot.motorsStop();
+      if (n%10==0){
+        Robot.text("Leftmost", 10, 50);
+      }
+      //Robot.motorsWrite(85, -75);
+      Robot.motorsWrite(-40, 85);
+//      delay(150);
+//      Robot.motorsStop();
+      
+      lastLeftMotor = -40;
+      lastRightMotor = 85;
       //Robot.turn(10);
     }
+    else {
+      Robot.motorsWrite(lastLeftMotor, lastRightMotor);
+    }
+   }
+   
    else{
-     Robot.motorsStop();
+      Robot.motorsWrite(lastLeftMotor, lastRightMotor);
    }
   }
- }
+
 
